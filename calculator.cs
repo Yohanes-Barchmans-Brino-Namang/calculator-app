5363115e6e6bd508131546b8161dca59e2b8ddde
Yohanes Barchmans Brino Namang <brinonamang@students.amikom.ac.id>
2025-03-11 14:14:48 +0700

    firstcomit

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,23 @@ class Calculator
 {
 	static void Main (string[]args)
 	{
+		int a= 10;
+		int b=6; 
+		
+		console.Writeline("haail penambahan: {0} + {1} ={2}",a,b,penambahan(a,b));
+		console.Writeline("haail pengurangan : {0} - {1} ={2}",a,b,pengurangan(a,b));
+		
+		console.Writeline("\nTekan sembarang key untuk keluar ");
+		console.Readkey();
 		
 	}
+	static int penambahan (int a, int b)
+	{
+		return a+b;
+	}
+	static int pengurangan (int a, int b)
+	{
+	return a-b ;
+	}	
+	
 }	
